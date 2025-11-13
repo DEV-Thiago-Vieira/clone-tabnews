111f334d11ffeb60eb616e31c18f1aaca4dfd927
Thiago Vieira <pessoalvieira779@gmail.com>
2025-11-13 22:27:03 +0000

    padding space added

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ function Home() {
                 `}</style>
             </Head>
             <div style={{ fontFamily: "'Poppins', sans-serif", display: "grid", placeItems: "center", height: "100vh", backgroundColor: "#1e1e1e" }}>
-                <main style={{ color: "#ffffff" }}>
+                <main style={{ color: "#ffffff", padding: "1rem" }}>
                     <span>Caro Devi,</span>
                     <h1>O nome <span style={{ fontWeight: "700", color: "red" }}>Devilman</span> é mil vezes melhor que <span style={{ fontWeight: "700", color: "gray" }}>ValkyriaX</span>.</h1>
                     <p>Atenciosamente, Samurai.</p>
