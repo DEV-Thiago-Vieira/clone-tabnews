d4cedefd76212e29164dd365bf0dce0d01c94671
Thiago Vieira <pessoalvieira779@gmail.com>
2025-11-14 15:32:41 +0000

    added `lint:check` and `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,49 @@
 import Head from "next/head";
 
 function Home() {
-    return (
-        <>
-            <Head>
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-                <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet" />
-                <style>{`
+  return (
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+        <style>{`
                     * {
                         margin: 0;
                         padding: 0;
                         box-sizing: border-box;
                     }
                 `}</style>
-            </Head>
-            <div style={{ fontFamily: "'Poppins', sans-serif", display: "grid", placeItems: "center", height: "100vh", backgroundColor: "#1e1e1e" }}>
-                <main style={{ color: "#ffffff", padding: "1rem" }}>
-                    <span>Caro Devi,</span>
-                    <h1>O nome <span style={{ fontWeight: "700", color: "red" }}>Devilman</span> é mil vezes melhor que <span style={{ fontWeight: "700", color: "gray" }}>ValkyriaX</span>.</h1>
-                    <p>Atenciosamente, Samurai.</p>
-                </main>
-            </div>
-        </>
-    );
+      </Head>
+      <div
+        style={{
+          fontFamily: "'Poppins', sans-serif",
+          display: "grid",
+          placeItems: "center",
+          height: "100vh",
+          backgroundColor: "#1e1e1e",
+        }}
+      >
+        <main style={{ color: "#ffffff", padding: "1rem" }}>
+          <span>Caro Devi,</span>
+          <h1>
+            O nome{" "}
+            <span style={{ fontWeight: "700", color: "red" }}>Devilman</span> é
+            mil vezes melhor que{" "}
+            <span style={{ fontWeight: "700", color: "gray" }}>ValkyriaX</span>.
+          </h1>
+          <p>Atenciosamente, Samurai.</p>
+        </main>
+      </div>
+    </>
+  );
 }
 
 export default Home;
